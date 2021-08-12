1af15cc2d3adde4c04b7e2dff9e70afa03c0bc34
uot0705 <uot2345906abc@gmail.com>
2021-08-12 13:34:21 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'uot0705' && password == '7777'
+    end
+  end
 end
