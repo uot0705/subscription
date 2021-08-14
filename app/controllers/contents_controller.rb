b18d0579d4c0330b319e78afb86e02e630f65570
uot0705 <uot2345906abc@gmail.com>
2021-08-14 18:10:44 +0900

    contentモデル・Active Storageの導入

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,9 @@
 class ContentsController < ApplicationController
   def index
   end
+
+  private
+  def content_params
+    params.require(:content).permit(:name, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,4 @@
 class Content < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
