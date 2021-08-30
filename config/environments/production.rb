87763e2054f01ea208a1cb489ffb195af9186845
uot0705 <uot2345906abc@gmail.com>
2021-08-30 16:15:37 +0900

    EC2メール修正3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 52.68.230.104 }
+  config.action_mailer.default_url_options = { host: "52.68.230.104" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
