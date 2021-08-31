4fc9da4645ede190c9f9f146a391ca83bc0b5fc3
uot0705 <uot2345906abc@gmail.com>
2021-08-31 15:38:58 +0900

    初期に戻す、定期実行

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
-require 'active_support/core_ext/time'
 set :path_env, ENV['PATH']
 job_type :runner, "cd :path && PATH=':path_env' bin/rails runner -e :environment ':task' :output"
-job_type :rake, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
+job_type :rake,   "cd :path && PATH=':path_env' :environment_variable=:environment bundle exec rake :task --silent :output"
 rails_env = ENV['RAILS_ENV'] || :development
-set :output, environment == 'development' ? 'log/crontab.log' : '/deploy/apps/subscription/shared/log/crontab.log'
+set :output, "#{Rails.root}/log/cron.log"
 set :environment, rails_env
 
 
 # 毎月1日の9:30に起動(月１)
- every 1.minutes do
+every 1.minutes do
   rake "thanxmailer_a:thanxmailer_a" 
 end
 
