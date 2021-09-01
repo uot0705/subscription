172255c9dd15c00d9dd29247fa5fa599a2ab2121
uot0705 <uot2345906abc@gmail.com>
2021-09-01 11:05:01 +0900

    whenever修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,8 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/environment')
 set :path_env, ENV['PATH']
 job_type :runner, "cd :path && PATH=':path_env' bin/rails runner -e :environment ':task' :output"
 job_type :rake,   "cd :path && PATH=':path_env' :environment_variable=:environment bundle exec rake :task --silent :output"
+rails_env = ENV['RAILS_ENV'] || :production
 set :output, "#{Rails.root}/log/cron.log"
-set :environment, :production
+set :environment, rails_env
 
 # 毎月1日の9:30に起動(月１)
 every 1.minutes do
