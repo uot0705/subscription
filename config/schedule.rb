7e7de5b7846bf33755d7265c11bc4db2209e3d82
uot0705 <uot2345906abc@gmail.com>
2021-09-01 20:56:21 +0900

    修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,11 +12,11 @@ every 1.minutes do
 end
 
 # 毎月1日の9:30に起動(月２)
-every '51 11 1 * *' do
+every '58 11 1 * *' do
   rake "thanxmailer_b:thanxmailer_b" 
 end
 
 # 毎月15日の9:30に起動(月２)
-every '53 11 1 * *' do
+every '57 11 1 * *' do
   rake "thanxmailer_c:thanxmailer_c" 
 end
