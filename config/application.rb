dece45d193013cacfaf1f62e7def5420baa37555
uot0705 <uot2345906abc@gmail.com>
2021-09-01 17:22:23 +0900

    whenever修正

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Subscription
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s] 
-    config.paths.add 'lib', eager_load: true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
