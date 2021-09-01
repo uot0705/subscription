59683f577794fc6803e4fd82270e7813c9d546ab
uot0705 <uot2345906abc@gmail.com>
2021-09-01 21:17:27 +0900

    時間修正テスト

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,17 +6,17 @@ rails_env = ENV['RAILS_ENV'] || :production
 set :output, "#{Rails.root}/log/cron.log"
 set :environment, rails_env
 
-# 毎月1日の9:30に起動(月１)
-every 1.minutes do
+# 毎月1日の9:30に起動(月１)下記時間から
+every  '30 0 2 * *' do
   rake "thanxmailer_a:thanxmailer_a" 
 end
 
 # 毎月1日の9:30に起動(月２)
-every '58 11 1 * *' do
-  rake "thanxmailer_b:thanxmailer_b" 
+every '31 0 2 * *' do
+  rake "thanxmailer_a:thanxmailer_b" 
 end
 
 # 毎月15日の9:30に起動(月２)
-every '57 11 1 * *' do
-  rake "thanxmailer_c:thanxmailer_c" 
+every '32 0 2 * *' do
+  rake "thanxmailer_a:thanxmailer_c" 
 end
