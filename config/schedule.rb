d875064167a08c3ecf143d45e44d154e87304a2b
uot0705 <uot2345906abc@gmail.com>
2021-09-02 11:28:31 +0900

    メールテスト

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,7 @@ set :output, "#{Rails.root}/log/cron.log"
 set :environment, rails_env
 
 # 毎月1日の9:30に起動(月１)下記時間から
-every  '59 1 2 * *' do
+every  '35 2 2 * *' do
   rake "thanxmailer_a:thanxmailer_a" 
 end
 
